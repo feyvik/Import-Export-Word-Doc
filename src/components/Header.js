62d722f1774b697f51bbef474516ae419d59223c
FayVik <vivianemma97@gmail.com>
2021-10-22 14:12:40 +0100

    kepp changes

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,12 +3,12 @@ import React from 'react';
 export default function Header() {
 	return (
 		<header>
-			<nav class='navbar navbar-expand-lg navbar-light bg-light px-4'>
-				<a class='navbar-brand' href='#home'>
+			<nav className='navbar navbar-expand-lg navbar-light bg-light px-4'>
+				<a className='navbar-brand' href='#home'>
 					Navbar
 				</a>
 				<button
-					class='navbar-toggler'
+					className='navbar-toggler'
 					type='button'
 					data-toggle='collapse'
 					data-target='#navbarSupportedContent'
@@ -16,18 +16,18 @@ export default function Header() {
 					aria-expanded='false'
 					aria-label='Toggle navigation'
 				>
-					<span class='navbar-toggler-icon'></span>
+					<span className='navbar-toggler-icon'></span>
 				</button>
 
-				<div class='collapse navbar-collapse' id='navbarSupportedContent'>
-					<ul class='navbar-nav mr-auto'>
-						<li class='nav-item active'>
-							<a class='nav-link' href='#home'>
+				<div className='collapse navbar-collapse' id='navbarSupportedContent'>
+					<ul className='navbar-nav mr-auto'>
+						<li className='nav-item active'>
+							<a className='nav-link' href='#home'>
 								Home
 							</a>
 						</li>
-						<li class='nav-item'>
-							<a class='nav-link' href='#home'>
+						<li className='nav-item'>
+							<a className='nav-link' href='#home'>
 								Link
 							</a>
 						</li>
--- src/components/MainContent.js
+++ src/components/MainContent.js
@@ -1,54 +1,36 @@
+import React, { useState } from 'react';
+import mammoth from 'mammoth';
+
 export default function MainContent() {
+	const [doc, setDoc] = useState();
+	const getFile = (e) => {
+		console.log(e);
+		var reader = new FileReader();
+		reader.onloadend = function (event) {
+			var arrayBuffer = reader.result;
+			mammoth
+				.convertToHtml({ arrayBuffer: arrayBuffer })
+				.then(function (resultObject) {
+					setDoc(resultObject.value);
+					console.log(resultObject);
+				});
+		};
+		reader.readAsArrayBuffer(e);
+	};
 	return (
 		<main>
 			<div className='row m-0 p-0'>
 				<div className='col-12'>
-					<div class='form-check'>
-						<input
-							type='checkbox'
-							class='form-check-input'
-							id='exampleCheck1'
-						/>
-						<label class='form-check-label' for='exampleCheck1'>
+					<div className='form-check'>
+						<input type='file' onChange={(e) => getFile(e.target.files[0])} />
+						<label className='form-check-label' htmlFor='exampleCheck1'>
 							Check me out
 						</label>
 					</div>
-				</div>
-				<div className='col-12'>
-					<div class='form-check'>
-						<input
-							type='checkbox'
-							class='form-check-input'
-							id='exampleCheck1'
-						/>
-						<label class='form-check-label' for='exampleCheck1'>
-							Check me out
-						</label>
-					</div>
-				</div>
-				<div className='col-12'>
-					<div class='form-check'>
-						<input
-							type='checkbox'
-							class='form-check-input'
-							id='exampleCheck1'
-						/>
-						<label class='form-check-label' for='exampleCheck1'>
-							Check me out
-						</label>
-					</div>
-				</div>
-				<div className='col-12'>
-					<div class='form-check'>
-						<input
-							type='checkbox'
-							class='form-check-input'
-							id='exampleCheck1'
-						/>
-						<label class='form-check-label' for='exampleCheck1'>
-							Check me out
-						</label>
-					</div>
+					<div
+						dangerouslySetInnerHTML={{ __html: doc }}
+						contentEditable='true'
+					></div>
 				</div>
 			</div>
 		</main>
